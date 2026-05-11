fa2e66621f7a59a2a2b9710dcba095ed9d327c7a
vidhi3000 <vdmaharwade@gmail.com>
2026-05-11 18:26:47 +0530

    Updated Resume

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
               </a>
             </Button>
             <Button variant="hero-outline" size="lg" asChild>
-              <a href="/VIDHI-RESUME.pdf" download>
+              <a href="/Vidhi Maharwade-Resume2026.pdf.pdf" download>
                 <FileText size={18} />
                 Download Resume
               </a>

1d2ecff6fcf2ba326fdf90759889265f501c7c38
vidhi3000 <vdmaharwade@gmail.com>
2026-05-11 18:31:46 +0530

    Fixed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -53,7 +53,7 @@ const Hero = () => {
               </a>
             </Button>
             <Button variant="hero-outline" size="lg" asChild>
-              <a href="/Vidhi Maharwade-Resume2026.pdf.pdf" download>
+              <a href="/Vidhi Maharwade-Resume2026.pdf" download>
                 <FileText size={18} />
                 Download Resume
               </a>
